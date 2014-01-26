47f45ce163f0c478b597395c315ffa27fe24133c
João Gonçalves <goncalves.joao@gmail.com>
2014-01-26 19:23:09 +0000

    introducing target_parent

--- lib/usecasing_validations/target.rb
+++ lib/usecasing_validations/target.rb
@@ -17,6 +17,7 @@ module UseCaseValidations
         end
 
         define_method(:target) { send(object_sym) }
+        define_method(:target_parent) { options.key?(:in) ? send(options[:in]) : nil }
       end
 
     end
--- lib/usecasing_validations/version.rb
+++ lib/usecasing_validations/version.rb
@@ -1,3 +1,3 @@
 module UseCaseValidations
-  VERSION = "0.4.1"
+  VERSION = "0.4.2"
 end
